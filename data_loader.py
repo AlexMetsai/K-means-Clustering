7246046ee0a0d2a9aa5405e223defc4cda87fe73
Alexandros Metsai <alexmetsai@gmail.com>
2020-07-05 12:13:56 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,20 @@
+# Alexandros Metsai
+# alexmetsai@gmail.com
 
+import pandas as pd
+
+def load_data(path = "data.csv"):
+    # Load data from csv.
+    data = pd.read_csv(path, header=None, names=["t",
+    "CPU",
+    "RxKBTot",
+    "TxKBTot",
+    "WriteKBTot",
+    "Watts",
+    "Amps",
+    "RMS",
+    "diff_encoder_l",
+    "flag"])
+    
+if __name__ == "__main__":
+    pass
